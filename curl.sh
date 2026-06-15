6a2504a0da561f3a3fc412ebb13a072fed70f210
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2026-06-16 01:09:49 +0900

    Delete curl.sh

--- curl.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:8080/invocations \
--H "Content-Type: application/json" \
--d '{"prompt": "내 s3 bucket 리스트는?", "mcp_servers": ["basic", "use_aws", "tavily-search", "filesystem", "terminal"], "model_name": "Claude 3.7 Sonnet"}'
